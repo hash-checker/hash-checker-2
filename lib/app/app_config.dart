1fb8f14fa15df9f184e5d95b568dd0cb59580759
fartem <jaman.smlnsk@gmail.com>
2022-01-02 16:56:47 +0300

    2022-01-02 v. 1.0.0: added Privacy Policy link

--- lib/app/app_config.dart
+++ lib/app/app_config.dart
@@ -1,5 +1,5 @@
 class AppConfig {
   static const String linkAuthor = 'https://github.com/fartem';
   static const String linkBugTracker = 'https://github.com/hash-checker/hash-checker-2/issues';
-  static const String linkPrivacyPolicy = '';
+  static const String linkPrivacyPolicy = 'https://github.com/hash-checker/hash-checker-2-privacy-policy.io';
 }
